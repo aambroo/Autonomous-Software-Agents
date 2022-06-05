8d5d62725da8d1f4b104866a2cb61179c9171bd8
aambroo <ambrosini.matteo21@gmail.com>
2022-06-05 10:44:51 +0200

    #ENHANCEMENT: added NumToDays.js to convert numeric days to calendar days

---
+++ src/utils/NumsToDays.js
@@ -0,0 +1,13 @@
+// Converts Clock.globals.dd into weekdays
+let NumsToDays;
+NumsToDays = Object.freeze({
+    'mon': 0,
+    'tue': 1,
+    'wed': 2,
+    'thu': 3,
+    'fri': 4,
+    'sat': 5,
+    'sun': 6,
+});
+
+module.exports = NumsToDays;
